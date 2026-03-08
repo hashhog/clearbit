cc8b1e16cae1f97e43ce3629fe65ce341bd1d1a5
max <maxtclinton@gmail.com>
2026-03-08 16:08:31 -0400

    export p2p module from main

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,7 @@ pub const script = @import("script.zig");
 pub const storage = @import("storage.zig");
 pub const consensus = @import("consensus.zig");
 pub const validation = @import("validation.zig");
+pub const p2p = @import("p2p.zig");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
@@ -23,4 +24,5 @@ test {
     _ = storage;
     _ = consensus;
     _ = validation;
+    _ = p2p;
 }
