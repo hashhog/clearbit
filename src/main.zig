a79dd0edd5e8b28753758642cb6ff267da12320e
max <maxtclinton@gmail.com>
2026-03-08 15:29:26 -0400

    wire up modules in main

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,16 @@
 const std = @import("std");
 
+pub const types = @import("types.zig");
+pub const crypto = @import("crypto.zig");
+pub const serialize = @import("serialize.zig");
+
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("clearbit - Bitcoin full node in Zig\n", .{});
+}
+
+test {
+    _ = types;
+    _ = crypto;
+    _ = serialize;
 }
